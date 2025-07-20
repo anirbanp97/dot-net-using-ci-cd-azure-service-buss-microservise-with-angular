67173b32169bfdf61d92e5a18c28d3b79281ffcd
anirbanp97 <anirbanp97@gmail.com>
2025-07-20 13:55:11 +0530

    Add project files.

---
+++ eCommerceSolution.UserService/eCommerce.Core/DTO/AuthenticationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public record AuthenticationResponse(
+  Guid UserID,
+  string? Email,
+  string? PersonName,
+  string? Gender,
+  string? Token,
+  bool Sucess
+  );
